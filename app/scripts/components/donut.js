b87442ccb9e1722f7be3da0141aadaedccccf703
Joe Zhang <yzhang4@cisco.com>
2016-08-26 16:11:31 +0800

    add stack chart

--- app/scripts/components/donut.js
+++ app/scripts/components/donut.js
@@ -2,7 +2,7 @@ $(function(){
     Highcharts.setOptions({
         colors: ['#b6dbf1', '#e9e9e9', '#97a1a2', '#24CBE5', '#64E572', '#FF9655', '#FFF263',      '#6AF9C4']
     });
-    $('#container').highcharts({
+    $('.container').highcharts({
         chart: {
             plotBackgroundColor: null,
             plotBorderWidth: 0,
@@ -56,4 +56,72 @@ $(function(){
             ]
         }]
     });
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
+
+        $('#stack').highcharts({
+            chart: {
+                zoomType: 'x',
+                backgroundColor: '#f9f9fb'
+                
+            },
+            credits:{
+              enabled:false
+            },
+            title: {
+                text: ''
+            },
+            /*subtitle: {
+                text: document.ontouchstart === undefined ?
+                        'Click and drag in the plot area to zoom in' : 'Pinch the chart to zoom in'
+            },*/
+            xAxis: {
+                type: 'datetime',
+                labels: {
+                    align: 'top',
+                    x: -2,
+                    y: 0
+                }
+            },
+            yAxis: {
+                title: {
+                    text: ''
+                },
+                labels: {
+                    align: 'left',
+                    x: 0,
+                    y: -2
+                },
+                minorGridLineWidth: 0,
+                gridLineWidth: 0,
+                alternateGridColor: null
+            },
+            legend: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillColor:'#b8d0e0',
+                    marker: {
+                        radius: 2
+                    },
+                    lineWidth: 1,
+                    states: {
+                        hover: {
+                            lineWidth: 1
+                        }
+                    },
+                    threshold: null
+                }
+            },
+						background2: '#0080ff',
+            series: [{
+                type: 'area',
+                name: 'USD to EUR',
+                data: data
+            }]
+        });
+    });
+
+
 })
